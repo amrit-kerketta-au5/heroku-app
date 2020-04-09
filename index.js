b7ebb7db5fe90905dc31fa459295c75c4b1263e4
AmritKerketta <amritkerketta90@gmail.com>
2020-04-10 00:19:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5000
 
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
 
-app.listen(5000,()=>{
-    console.log("server started on port 5000")
+app.listen(port,()=>{
+    console.log("server started on port "+port)
 })
