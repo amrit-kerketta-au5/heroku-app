6d8587500283a51f3c4e34598f35ea77a9284702
amrit-kerketta-au5 <amritkerketta90@gmail.com>
2020-06-14 04:17:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000 
+const port = process.env.PORT
 const path = require('path')
 app.get('/',async(req,res)=>{
     app.use(express.static('couples/build'))
